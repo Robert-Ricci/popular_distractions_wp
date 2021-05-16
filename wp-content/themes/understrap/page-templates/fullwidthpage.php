c03882abf1982e2c0477a1e1152259a36d534ebd
Robert Ricci <rmricci121@gmail.com>
2021-05-16 10:33:58 -0400

    This commit added custom fields to the template

--- wp-content/themes/understrap/page-templates/fullwidthpage.php
+++ wp-content/themes/understrap/page-templates/fullwidthpage.php
@@ -29,19 +29,10 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 				<main class="site-main" id="main" role="main">
 
-					<?php while ( have_posts() ) : the_post(); ?>
-
-						<?php get_template_part( 'loop-templates/content', 'page' ); ?>
-
-						<?php
-						// If comments are open or we have at least one comment, load up the comment template.
-						if ( comments_open() || get_comments_number() ) :
-							comments_template();
-						endif;
-						?>
-
-					<?php endwhile; // end of the loop. ?>
-
+				<h1><?php the_field('name'); ?></h1> 
+				<h4><?php the_field('email'); ?></h4> 
+				<p><?php the_field('bio'); ?></p> 
+				
 				</main><!-- #main -->
 
 			</div><!-- #primary -->
